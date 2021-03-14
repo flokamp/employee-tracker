505a4eb59946cd10dda1b319e783b08c93b825ba
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-13 22:05:21 -0600

    inquirer prompts

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,33 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
 CREATE TABLE departments (
-  id INTEGER PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INTEGER PRIMARY KEY,
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY (id),
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-  id INTEGER PRIMARY KEY,
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER UNSIGNED NOT NULL,
   manager_id INTEGER UNSIGNED,
+  PRIMARY KEY (id),
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,21 +1,102 @@
-const express = require("express");
-const db = require("./db/database");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+require("console.table");
+var values = [
+	["max", 20],
+	["joe", 30],
+];
+console.table(values);
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-	res.status(404).end();
+const connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "!d0ntk0w",
+	database: "company_db",
 });
 
-// Start server after DB connection
-db.on("open", () => {
-	app.listen(PORT, () => {
-		console.log(`Server running on port ${PORT}`);
-	});
+connection.connect((err) => {
+	if (err) throw err;
+	optionsPrompt();
 });
+
+function optionsPrompt() {
+	inquirer
+		.prompt({
+			type: "list",
+			name: "options",
+			message: "What would you like to do?",
+			choices: [
+				"View all employees",
+				"View employees by department",
+				"View employees by manager",
+				"Add a department",
+				"Add a role",
+				"Add an employee",
+				"Update an employee role",
+			],
+		})
+		.then((answer) => {
+			switch (answer.options) {
+				case "View all employees":
+					viewAllEmployees();
+					break;
+				case "View employees by department":
+					viewAllEmployees();
+					break;
+				case "View employees by manager":
+					viewByDepartment();
+					break;
+				case "Add a department":
+					addDepartment();
+					break;
+				case "Add a role":
+					addRole();
+					break;
+				case "Add an employee":
+					addEmployee();
+					break;
+				case "Update an employee role":
+					updateEmployee();
+					break;
+			}
+		});
+}
+
+// function to create department from user input
+addDepartment = () => {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "What is the department name?",
+				validate: (answer) => {
+					if (answer) {
+						return true;
+					} else {
+						console.log("Please enter your department name");
+						return false;
+					}
+				},
+			},
+		])
+		.then((answer) => {
+			connection.query(`INSERT INTO departments SET ?`, {
+				department_name: answer.name,
+			}),
+				function (err, res) {
+					if (err) throw err;
+					const query = connection.query(
+						"SELECT * FROM departments",
+						// Include the callback function to catch any errors,
+						function (err, res) {
+							if (err) throw err;
+						}
+					);
+					console.table(rows);
+				};
+		});
+};
