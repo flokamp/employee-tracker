8e02bb942984fd7af395c9aa3fb7cd1a92da2302
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-14 13:12:09 -0500

    manager id for employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,34 @@
 INSERT INTO departments (department_name)
-VALUES (?)
+VALUES
+  ('Design'),
+  ('Data Science'),
+  ('Engineering'),
+  ('Sales and Consulting'),
+  ('Marketing'),
+  ('Finance');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES (?)
+VALUES
+  ('Graphic Design', '120000', 1),
+  ('Experience Design', '170000', 1),
+  ('Tech SEO Manager', '200000', 2),
+  ('Data Analyst', '200000', 2),
+  ('Front End Developer', '150000', 3),
+  ('Back End Developer', '150000', 3),
+  ('Customer Success', '120000', 4),
+  ('Program Manager', '200000', 4),
+  ('Marketing Manager', '100000', 5),
+  ('Content Writer', '100000', 5);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES (?)
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 2),
+  ('Piers', 'Gaveston', 4),
+  ('Charles', 'LeRoi', 3),
+  ('Katherine', 'Mansfield', 5),
+  ('Dora', 'Carrington', 6),
+  ('Edward', 'Bellamy', 3),
+  ('Montague', 'Summers', 5),
+  ('Octavia', 'Butler', 5),
+  ('Unica', 'Zurn', 4);
--- server.js
+++ server.js
@@ -22,9 +22,9 @@ function optionsPrompt() {
 			name: "options",
 			message: "What would you like to do?",
 			choices: [
+				"View all departments",
+				"View all roles",
 				"View all employees",
-				"View employees by department",
-				"View employees by manager",
 				"Add a department",
 				"Add a role",
 				"Add an employee",
@@ -37,10 +37,10 @@ function optionsPrompt() {
 					viewAllEmployees();
 					break;
 				case "View employees by department":
-					viewAllEmployees();
+					viewByDepartment();
 					break;
 				case "View employees by manager":
-					viewByDepartment();
+					viewByManager();
 					break;
 				case "Add a department":
 					addDepartment();
@@ -57,6 +57,29 @@ function optionsPrompt() {
 			}
 		});
 }
+
+viewAllEmployees = () => {
+	connection.query("SELECT * FROM employees", function (err, res) {
+		if (err) throw err;
+		console.table(res);
+		optionsPrompt();
+	});
+};
+
+viewByDepartment = () => {
+	const sql = `SELECT employees.*, departments.department_id
+                AS department_name
+                FROM employees
+                LEFT JOIN departments
+                ON employees.department_id = derpartments.id`;
+
+	connection.query(sql, (err, rows, fields) => {
+		if (err) throw err;
+		console.table(rows);
+	});
+};
+
+viewByManager = () => {};
 
 // function to create department from user input
 addDepartment = () => {
@@ -90,3 +113,65 @@ addDepartment = () => {
 			);
 		});
 };
+
+addRole = () => {};
+addEmployee = () => {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "firstName",
+				message: "What is the employees first name?",
+				validate: (answer) => {
+					if (answer) {
+						return true;
+					} else {
+						console.log("Please enter the employees first name");
+						return false;
+					}
+				},
+			},
+			{
+				type: "input",
+				name: "lastName",
+				message: "What is the employees last name?",
+				validate: (answer) => {
+					if (answer) {
+						return true;
+					} else {
+						console.log("Please enter the employees last name");
+						return false;
+					}
+				},
+			},
+			{
+				type: "list",
+				name: "employeeRole",
+				message: "What is their role?",
+				choices: [
+					"View all employees",
+					"View employees by department",
+					"View employees by manager",
+					"Add a department",
+					"Add a role",
+					"Add an employee",
+					"Update an employee role",
+				],
+			},
+		])
+		.then((answer) => {
+			connection.query(
+				`INSERT INTO departments SET ?`,
+				{
+					department_name: answer.name,
+				},
+				function (err, res) {
+					if (err) throw err;
+					console.log(answer.name + " successfully added!");
+					optionsPrompt();
+				}
+			);
+		});
+};
+
+updateEmployee = () => {};
