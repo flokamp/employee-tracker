13b1f4c4219ac671433328e51a6a422f10ef2c03
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-13 19:08:14 -0600

    created department table

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (department_name)
+VALUES
+  ('Design'),
+  ('Development'),
+  ('Implementation'),
+  ('Support'),
+  ('Sales'),
+  ('Marketing');
